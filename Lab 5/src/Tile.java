9f897f902c5debecfdde6b90eb514eca83ae272c
Darin Critchlow <darincritchlow@gmail.com>
2014-10-20 09:56:15 -0600

    Add main method for testing

--- Lab 5/src/Tile.java
+++ Lab 5/src/Tile.java
@@ -1,10 +1,27 @@
-import java.util.Objects;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
-public abstract class Tile
+public abstract class Tile extends JPanel
 {
 	public boolean matches(Tile other)
 	{
 		return this.getClass().equals(other.getClass());
+	}
+	
+	public static void main(String[] args)
+	{
+		JFrame	frame = new JFrame();
+
+		frame.setLayout(new FlowLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Tile");
+
+		frame.add(new Tile());
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
