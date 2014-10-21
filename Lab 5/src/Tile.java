abf16c159084630e9f78f9a49a98f7b4ce01e7ed
Darin Critchlow <darincritchlow@gmail.com>
2014-10-21 11:59:00 -0600

    Adding the two sides

--- Lab 5/src/Tile.java
+++ Lab 5/src/Tile.java
@@ -8,6 +8,16 @@ import javax.swing.JPanel;
 
 public class Tile extends JPanel
 {
+//	static
+//	{
+			int[] xPoints = {20, 20, 25, 25};
+			int[] yPoints = {90, 30, 20,80};
+			Polygon tileBottom = new Polygon(xPoints, yPoints, 4);
+			int[] xPointsMid = {30, 30, 35, 35};
+			int[] yPointsMid = {80, 20, 10,70};
+			Polygon tileMiddle = new Polygon(xPointsMid, yPointsMid, 4);
+//	}
+	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
@@ -18,14 +28,16 @@ public class Tile extends JPanel
 //		
 //		g.drawPolygon(xpoints, ypoints, npoints);
 		
-		g.drawRect(20, 50, 5, 75);
-		g.drawRect(7, 10, 80, 70);
+//		g.drawRect(20, 50, 5, 75);
+//		g.drawRect(7, 10, 80, 70);
 		
-		int[] xpoints = {20, 20, 30, 30};
-		int[] ypoints = {80, 20, 10,70};
-		Polygon mySide = new Polygon(xpoints, ypoints, 4);
-		g.drawPolygon(mySide);
+//		int[] xpoints = {20, 20, 30, 30};
+//		int[] ypoints = {80, 20, 10,70};
+//		Polygon mySide = new Polygon(xpoints, ypoints, 4);
+//		g.drawPolygon(mySide);
 		
+		g.drawPolygon(tileBottom);
+		g.drawPolygon(tileMiddle);
 		
 		Graphics2D	g2 = (Graphics2D)g;
 //		GradientPaint	grad = new GradientPaint(50, 50, Color.WHITE,
