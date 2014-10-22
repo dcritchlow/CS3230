0fbd1b53def1647c922254c7a2ce204cea7b9a6f
Darin Critchlow <darincritchlow@gmail.com>
2014-10-22 07:12:06 -0600

    Add main method to test

--- Lab 5/src/CharacterTile.java
+++ Lab 5/src/CharacterTile.java
@@ -1,5 +1,9 @@
 import java.util.Objects;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 
 public class CharacterTile extends Tile
 {
@@ -54,5 +58,39 @@ public class CharacterTile extends Tile
 		{
 			return "Invalid Character symbol";
 		}
+	}
+	
+	public static void main(String[] args)
+	{
+		JFrame		frame = new JFrame();
+		JPanel		tiles = new JPanel();
+		JScrollPane	scroller = new JScrollPane(tiles);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Character Tiles");
+		frame.add(scroller);
+
+		// Try something like this if your tiles don't fit on the screen.
+		// Replace "tile width" and "tile height" with your values.
+		// scroller.setPreferredSize(new Dimension(8 * 120, 40 + 110));
+
+		tiles.add(new CharacterTile('1'));
+		tiles.add(new CharacterTile('2'));
+		tiles.add(new CharacterTile('3'));
+		tiles.add(new CharacterTile('4'));
+		tiles.add(new CharacterTile('5'));
+		tiles.add(new CharacterTile('6'));
+		tiles.add(new CharacterTile('7'));
+		tiles.add(new CharacterTile('8'));
+		tiles.add(new CharacterTile('9'));
+		tiles.add(new CharacterTile('N'));
+		tiles.add(new CharacterTile('E'));
+		tiles.add(new CharacterTile('W'));
+		tiles.add(new CharacterTile('S'));
+		tiles.add(new CharacterTile('C'));
+		tiles.add(new CharacterTile('F'));
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
